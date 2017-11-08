2d295fbbf5f6976c3e95463acffd980fde53bfc0
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-08 16:12:51 +0000

    Tests passing

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -7,11 +7,11 @@ describe Battle do
       visit '/'
       expect(page).to have_content "Testing infrastructure working!"
     end
-    xit 'should display names of the players' do
+    it 'should display names of the players' do
       visit '/'
       fill_in 'player1', with: "JJ"
       fill_in 'player2', with: "Marie"
-      click_button 'Submit'
+      click_button 'Save'
       expect(page).to have_content "JJ"
     end
   end
