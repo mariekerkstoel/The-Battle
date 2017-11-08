b07f3a2520a84317d6969f27a89a0a76ceca242b
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-08 16:04:28 +0000

    Added form, added names displayed, added tests

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -7,6 +7,12 @@ describe Battle do
       visit '/'
       expect(page).to have_content "Testing infrastructure working!"
     end
+    xit 'should display names of the players' do
+      visit '/'
+      fill_in 'player1', with: "JJ"
+      fill_in 'player2', with: "Marie"
+      click_button 'Submit'
+      expect(page).to have_content "JJ"
+    end
   end
-
 end
