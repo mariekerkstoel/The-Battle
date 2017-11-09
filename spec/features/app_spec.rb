c20238bc513e20e09d68dfa11c5e37eefc8ce756
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-09 12:57:51 +0000

    Added Capybara test

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -24,6 +24,12 @@ describe Battle do
       click_button 'Attack'
       expect(page).to have_content('JJ attacked Marie!')
     end
+    it 'should display 50 points after the attack' do
+      sign_in_and_play
+      click_button 'Attack'
+      click_button 'Back to game'
+      expect(page).to have_content "Player 2: 50 HP"
+    end
   end
 
 end
