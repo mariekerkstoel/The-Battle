295f7cd7650257e8538422ef030d643e23051ba0
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-10 11:10:53 +0000

    Added winning confirmation

--- app.rb
+++ app.rb
@@ -20,12 +20,21 @@ class Battle < Sinatra::Base
     @points1 = $game.player1.hitpoints
     @points2 = $game.player2.hitpoints
     @count = $game.count
+    if @points2 <= 0 || @points1 <= 0
+      redirect('/winner')
+    end
     erb(:play)
   end
 
+  get '/winner' do
+    @player1 = $game.player1.name
+    @player2 = $game.player2.name
+    @points1 = $game.player1.hitpoints
+    @points2 = $game.player2.hitpoints
+    erb(:winner)
+  end
+
   post '/attack_player2' do
-    # @player1 = $game.player1.name
-    # @player2 = $game.player2.name
     $game.player2.hitpoints > 0
     $game.attack_player2
     $game.switch_turn
@@ -33,8 +42,6 @@ class Battle < Sinatra::Base
   end
 
   post '/attack_player1' do
-    # @player1 = $game.player1.name
-    # @player2 = $game.player2.name
     $game.player1.hitpoints > 0
     $game.attack_player1
     $game.switch_turn
